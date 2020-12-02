4a56cfcd74f0b1495c3f3b5558ccb8aee84d3c5b
Yuichiro Smith <contact@yu-smith.com>
2020-12-02 15:24:07 +0900

    1.0.0

--- git_id/__init__.py
+++ git_id/__init__.py
@@ -2,7 +2,7 @@ from .profile import *
 from .core import git_id
 
 __copyright__ = 'Copyright (C) 2020 Yuichiro Smith'
-__version__ = '0.1.0'
+__version__ = '1.0.0'
 __license__ = 'MIT License'
 __author__ = 'Yuichiro Smith'
 __author_email__ = 'contact@yu-smith.com'
