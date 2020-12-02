15eb37e8c22eb87c3158ef283f64b7da20fd90ac
Yuichiro Smith <contact@yu-smith.com>
2020-12-02 23:41:38 +0900

    bump patch v1.1.2

--- git_id/__init__.py
+++ git_id/__init__.py
@@ -2,7 +2,7 @@ from git_id.profile import *
 from git_id.core import main
 
 __copyright__ = 'Copyright (C) 2020 Yuichiro Smith'
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 __license__ = 'MIT License'
 __author__ = 'Yuichiro Smith'
 __author_email__ = 'contact@yu-smith.com'
